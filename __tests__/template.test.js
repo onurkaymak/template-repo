c24d3a726664cd9e3f48ef18ca58b4741cf3abe6
onurkaymak <onurkaymak@outlook.com>
2023-08-30 06:02:34 -0700

    Add __tests__ and coverage folders for JEST

---
+++ __tests__/template.test.js
@@ -0,0 +1,8 @@
+// import { Function } from './../src/js/template.js';
+
+// describe('Function GOES HERE', () => {
+
+//   test('EXPLAIN YOUR TEST HERE', () => {
+//     // expect().toEqual(result);
+//   });
+// });
